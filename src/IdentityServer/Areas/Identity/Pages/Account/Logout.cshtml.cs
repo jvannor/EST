98c4348c1b0aa6dad62bf52ab358a552fcaa2440
Jeff Van Nortwick <jeffvan@microsoft.com>
2021-12-03 11:20:43 -0500

    adjusting logout functionality based on Duende sample

--- src/IdentityServer/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ src/IdentityServer/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,9 +1,8 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Duende.IdentityServer.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,31 +11,50 @@ using Microsoft.Extensions.Logging;
 
 namespace IdentityServer.Areas.Identity.Pages.Account
 {
+    [AllowAnonymous]
     public class LogoutModel : PageModel
     {
+        private readonly IIdentityServerInteractionService _interactionService;
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(IIdentityServerInteractionService interactionService, SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
         {
+            _interactionService = interactionService;
             _signInManager = signInManager;
             _logger = logger;
         }
 
-        public async Task<IActionResult> OnPost(string returnUrl = null)
+        public async Task<IActionResult> OnGet(string logoutId)
         {
+            var request = await _interactionService.GetLogoutContextAsync(logoutId);
+            if (request?.ShowSignoutPrompt == false || !User.Identity.IsAuthenticated)
+            {
+                return await OnPost(logoutId);
+            }
+
+            return Page();
+        }
+
+        public bool LoggedOut { get; set; }
+        public string PostLogoutRedirectUri { get; set; }
+        public string SignOutIframeUrl { get; set; }
+
+        public async Task<IActionResult> OnPost(string logoutId)
+        {
+            LoggedOut = true;
+
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
+
+            var request = await _interactionService.GetLogoutContextAsync(logoutId);
+            if (request != null)
             {
-                return LocalRedirect(returnUrl);
+                PostLogoutRedirectUri = request.PostLogoutRedirectUri;
+                SignOutIframeUrl = request.SignOutIFrameUrl;
             }
-            else
-            {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
-            }
+
+            return Page();
         }
     }
 }
