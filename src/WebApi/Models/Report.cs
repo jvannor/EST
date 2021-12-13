45b1fb320888ae387fe1da97d09fbf0832dfb526
jvannor <jvannor@hotmail.com>
2021-12-13 13:25:27 -0500

    added to report

--- src/WebApi/Models/Report.cs
+++ src/WebApi/Models/Report.cs
@@ -16,6 +16,12 @@
 
         public int Revision { get; set; }
 
+        public string Category { get; set; }
+
+        public string Subcategory { get; set; }
+
+        public List<string> Tags { get; private set; } 
+
         public string Description { get; set; }
     }
 }
